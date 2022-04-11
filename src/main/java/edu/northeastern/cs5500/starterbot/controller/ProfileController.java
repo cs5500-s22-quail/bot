d7d8fcf1e5ab6b888c65ac47f8250cee6eac9b1c
shengchen1 <464463779@qq.com>
2022-04-11 15:45:37 -0700

    initial profile command, TBD

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package edu.northeastern.cs5500.starterbot.controller;
+
+import javax.inject.Inject;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+public class ProfileController {
+
+    @Inject
+    public ProfileController() {}
+
+    public EmbedBuilder getProfile(
+            String discordUserId, String discordAvatarUrl, String PreferredName) {
+        int balance = 0; // the balance, initially set to 0. TBD
+        int pokemons = 0; // the captured pokemons, initially set to 0. TBD
+        int released = 0; // the released pokemons, initially set to 0. TBD
+        EmbedBuilder eb =
+                new EmbedBuilder()
+                        .setTitle(PreferredName + "'s Profile")
+                        .setDescription(
+                                "Balance: "
+                                        + String.valueOf(balance)
+                                        + "\nCaptured Pokemons: "
+                                        + String.valueOf(pokemons)
+                                        + "\nReleased Pokemons: "
+                                        + String.valueOf(released)
+                                        + "\nTotal Pokemons: "
+                                        + String.valueOf(pokemons + released));
+
+        return eb.setThumbnail(discordAvatarUrl);
+    }
+}
