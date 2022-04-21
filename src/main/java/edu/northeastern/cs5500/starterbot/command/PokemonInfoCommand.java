d420d051ebf7297e196cbd54fa22e20b52f9d76e
huc008 <chenhy997@gmail.com>
2022-04-21 00:11:10 -0700

    implement fight feature under train command and allPokemon command and pokemonInfo command features

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/PokemonInfoCommand.java
@@ -0,0 +1,41 @@
+package edu.northeastern.cs5500.starterbot.command;
+
+import edu.northeastern.cs5500.starterbot.controller.PokemonInfoController;
+import edu.northeastern.cs5500.starterbot.controller.UserPokemonController;
+import edu.northeastern.cs5500.starterbot.model.PokemonInfo;
+import edu.northeastern.cs5500.starterbot.model.UserPokemon;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+@Singleton
+@Slf4j
+public class PokemonInfoCommand implements Command {
+
+    @Inject UserPokemonController userPokemonController;
+
+    @Inject
+    public PokemonInfoCommand() {}
+
+    @Override
+    public String getName() {
+        return "pokemoninfo";
+    }
+
+    @Override
+    public CommandData getCommandData() {
+        return new CommandData(getName(), "Ask the bot to reply with the carried pokemon info");
+    }
+
+    @Override
+    public void onEvent(CommandInteraction event) {
+        log.info("event: /pokemoninfo");
+        UserPokemon userPokemon =
+                userPokemonController.getUserPokemonForMemberID(event.getUser().getId());
+        PokemonInfo userPokeInfo = userPokemon.getCarriedPokemon();
+        PokemonInfoController pokemonInfoController = new PokemonInfoController(userPokeInfo);
+        event.replyEmbeds(pokemonInfoController.getPokemonInfoEmbed().build()).queue();
+    }
+}
--- src/main/java/edu/northeastern/cs5500/starterbot/listener/EmbedBuilderGenerator.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/listener/EmbedBuilderGenerator.java
@@ -4,7 +4,6 @@ import edu.northeastern.cs5500.starterbot.controller.PokemonGenerator;
 import edu.northeastern.cs5500.starterbot.controller.UserPokemonController;
 import edu.northeastern.cs5500.starterbot.model.PokemonInfo;
 import edu.northeastern.cs5500.starterbot.model.UserPokemon;
-import edu.northeastern.cs5500.starterbot.model.WildPokemon;
 import java.util.ArrayList;
 import javax.inject.Inject;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -15,30 +14,6 @@ public class EmbedBuilderGenerator {
 
     @Inject
     public EmbedBuilderGenerator() {}
-
-    public EmbedBuilder getFightPokemonEmbeds() {
-        WildPokemon fightPokemon = pokemonGenerator.getWildPokemon();
-        PokemonInfo fightPokemonInfo = fightPokemon.getPokemonInfo();
-        String officialArtworkUrl = fightPokemonInfo.getOfficialArtworkUrl();
-
-        EmbedBuilder embedBuilder =
-                new EmbedBuilder()
-                        .setTitle(fightPokemonInfo.getName() + " is fighting with your pokemon...")
-                        .setDescription(
-                                // TODO: change attributes for /fight usage
-                                //                                        +
-                                // ivUIBundle(fightPokemonInfo)
-                                "HP: "
-                                        + fightPokemonInfo.getHp()
-                                        + " | Level: "
-                                        + fightPokemonInfo.getLevel())
-                        .setImage(officialArtworkUrl);
-        return embedBuilder;
-    }
-
-    private String ivUI(String statName, int baseStat, int iv) {
-        return "\n" + statName + ": " + baseStat + " - IV: " + iv + "/31";
-    }
 
     public EmbedBuilder getAllPokemonEmbeds(String userId) {
         UserPokemon userPokemon = userPokemonController.getUserPokemonForMemberID(userId);
